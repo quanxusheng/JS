53a542646dd3f382550c00b0aeaa3c6f9716dfca
fangyiyuan <466973739@qq.com>
2023-05-16 18:00:20 +0800

    tj

---
+++ vue3-demo/vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import html2canvas from 'html2canvas'
+
+const app: HTMLElement | null = document.getElementById('app')
+console.log('=>', app)
+html2canvas(document.body).then(res => {
+    console.log('=>', res)
+})
+</script>
+
+<template>
+
+    <div id="app">
+        <div class="btn-shot">
+            截图
+        </div>
+        <div id="screen-area">
+            2222
+        </div>
+        <teleport to="body">
+            <canvas
+                id="screenShotContainer"
+                :width="screenShortWidth"
+                :height="screenShortHeight"
+                ref="screenShortController"
+            >
+
+            </canvas>
+        </teleport>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
